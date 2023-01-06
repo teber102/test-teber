a17bb1006279834a0c66d034ff7a48e0830cdad0
teber <teber.rb1998@gmail.com>
2023-01-06 03:03:30 +0100

    version2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { PostListComponent } from './post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+    {path:'authentifivation',component:AuthentificationComponent},
+    {path:'post-item',component:PostItemComponent},
+    {path:'post-list',component:PostListComponent},
+    
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostListComponent,
+    PostItemComponent,
+    AuthentificationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
